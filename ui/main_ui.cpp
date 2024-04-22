e389a33c58263f81fff3c195c7b3baea4fefa895
TNT <86717030+GGTNT@users.noreply.github.com>
2024-04-22 15:21:35 +0200

    Add files via upload

---
+++ ui/main_ui.cpp
@@ -0,0 +1,5 @@
+//
+// Created by theo on 22/04/24.
+//
+
+#include "main_ui.h"
---
+++ ui/main_ui.h
@@ -0,0 +1,14 @@
+//
+// Created by theo on 22/04/24.
+//
+
+#ifndef SYNCOPIA_MAIN_UI_H
+#define SYNCOPIA_MAIN_UI_H
+
+
+class main_ui {
+
+};
+
+
+#endif //SYNCOPIA_MAIN_UI_H
